377861cba3569433ecf15d0606c688b295e93ba6
intAV <38396198+intAV@users.noreply.github.com>
2023-01-10 02:11:36 +0800

    Add files via upload

---
+++ resnet50/resnet_train.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+import my_datasets
+from resnet50.resnet import myresnet
+
+max_epoch = 20
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if __name__ == '__main__':
+    train_datas=my_datasets.mydatasets("../dataset/train")
+    test_data=my_datasets.mydatasets("../dataset/test")
+    train_dataloader=DataLoader(train_datas,batch_size=64,shuffle=True)
+    # test_dataloader = DataLoader(test_data, batch_size=32, shuffle=True)
+
+    m=myresnet().to(device)
+    loss_fn=nn.MultiLabelSoftMarginLoss().to(device)
+    optimizer = torch.optim.Adam(m.parameters(), lr=0.001)
+    writer=SummaryWriter("logs")
+    w=SummaryWriter("logs")
+    total_step=0
+
+for epoch in range(max_epoch):
+    print("外层训练次数{}".format(epoch+1))
+    for i,(imgs,targets) in enumerate(train_dataloader):
+        imgs=imgs.to(device)
+        targets=targets.to(device)
+        outputs=m(imgs)
+        loss = loss_fn(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_step += 1
+        print("训练{}次,loss:{}".format(total_step * 10, loss.item()))
+        w.add_scalar("loss", loss, total_step)
+
+    writer.close()
+    # tensorboard --logdir=logs
+
+torch.save(m.state_dict(), "../checkpoints/res-model.pth")
